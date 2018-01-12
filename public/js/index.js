d8134149b75a414820a6631261f53d0af309af34
Libinquan <2578326021@qq.com>
2018-01-12 17:56:39 +0800

    Update index.js

--- public/js/index.js
+++ public/js/index.js
@@ -2,4 +2,7 @@ import {Home} from './containers'
 import React from 'react'
 import {render} from 'react-dom'
 
+
+let a = 10;
+const fn = ()=> {};
 render(<Home />,gitdocument.querySelector('#app'));
