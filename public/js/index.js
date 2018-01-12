fa51b939087c97e33be56e3a309ed74cde472035
lbq <2578326021@qq.com>
2018-01-12 17:50:02 +0800

    test

--- public/js/index.js
+++ public/js/index.js
@@ -2,6 +2,6 @@ import {Home} from './containers'
 import React from 'react'
 import {render} from 'react-dom'
 
-let a = 1, b = 10;
+let a = 1, aa = 10;
 
 render(<Home />,gitdocument.querySelector('#app'));
