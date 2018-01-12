c500b08e6f83f4e64fc1bd33fcde882e392a9f4e
嘉嘉 <jiajia@jiajiadeMacBook-Pro.local>
2018-01-12 15:40:05 +0800

    myStudy

--- public/js/index.js
+++ public/js/index.js
@@ -2,4 +2,4 @@ import {Home} from './containers'
 import React from 'react'
 import {render} from 'react-dom'
 
-render(<Home />, document.querySelector('#app'))
+render(<Home />, document.querySelector('#app'));
